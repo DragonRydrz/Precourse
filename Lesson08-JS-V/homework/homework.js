21e73dc0bbdf97cc63cd3e6c7ecc2287907cbbe4
AJ Genung <DragonRydrz@me.com>
2018-04-14 22:09:02 -0400

    8 partial, 9 complete

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -1,12 +1,34 @@
 // Do not change any of the function names
 
-function createUserClass() {
+function createUserClass(options) {
   // create an ES6 class or a constructor function called User
   // it should accept an options object with username, name, email, and password properties
   // in the constructor set the username, name, email, and password properties
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the class
+  // const User = function(options) {
+  //   this.name = options.name;
+  //   this.email = options.email;
+  //   this.username = options.username;
+  //   this.password = options.password;
+  //   this.sayHi = function() {
+  //     return `Hello, my name is ${this.name}`;
+  //   };
+  //   return User;
+  // };
+  class User {
+    constructor(options) {
+      this.name = options.name;
+      this.email = options.email;
+      this.username = options.username;
+      this.password = options.password;
+    }
+  }
+  User.prototype.sayHi = function() {
+    return `Hello, my name is ${this.name}`;
+  };
+  return User;
 }
 
 function addPrototypeMethod(Constructor) {
